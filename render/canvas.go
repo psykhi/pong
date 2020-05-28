6752b3055488c39b710d41058080558b5fde22c6
psykhi <alex@pesant.me>
2020-05-28 21:53:22 +0200

    DARK

--- render/canvas.go
+++ render/canvas.go
@@ -14,6 +14,11 @@ type Canvas struct {
 	id string
 }
 
+const TextColor = "white"
+
+//const BackgroundColor = "black"
+const BallColor = "white"
+
 func NewCanvas(id string, width, height float64) *Canvas {
 	canvas := js.
 		Global().
@@ -39,7 +44,6 @@ func (c *Canvas) Resize(w, h float64) {
 }
 
 func (c *Canvas) Render(s game.State) {
-	//fmt.Println(c.W, c.H)
 	c.context().Call("clearRect", 0, 0, c.W, c.H)
 	if s.Finished {
 		c.drawFinished()
@@ -65,7 +69,7 @@ func (c *Canvas) Render(s game.State) {
 func (c *Canvas) drawPing(p1 time.Duration, p2 time.Duration) {
 	c.context().Set("font", "20px Arial")
 	c.context().Set("textBaseline", "top")
-	c.context().Set("fillStyle", "black")
+	c.context().Set("fillStyle", TextColor)
 	c.context().Set("textAlign", "left")
 	c.context().Call("fillText", formatPing(p1), 0.0, 0)
 	c.context().Set("textAlign", "right")
@@ -78,14 +82,14 @@ func formatPing(p time.Duration) string {
 
 func (c *Canvas) drawWaiting() {
 	c.context().Set("font", "50px Arial")
-	c.context().Set("fillStyle", "black")
+	c.context().Set("fillStyle", TextColor)
 	c.context().Set("textAlign", "center")
 	c.context().Call("fillText", "Waiting for opponent...", 0.5*c.W, c.H/2)
 }
 
 func (c *Canvas) drawFinished() {
 	c.context().Set("font", "30px Arial")
-	c.context().Set("fillStyle", "black")
+	c.context().Set("fillStyle", TextColor)
 	c.context().Set("textAlign", "center")
 	c.context().Call("fillText", "Opponent left or connection was lost. Refresh to look for game", 0.5*c.W, c.H/2)
 }
@@ -101,7 +105,7 @@ func (c *Canvas) context() js.Value {
 
 func (c *Canvas) drawScore(p1 int, p2 int) {
 	c.context().Set("font", "50px Arial")
-	c.context().Set("fillStyle", "black")
+	c.context().Set("fillStyle", TextColor)
 	c.context().Set("textAlign", "center")
 	c.context().Set("textBaseline", "top")
 	c.context().Call("fillText", p1, c.W/4, 0.05*c.H)
@@ -110,6 +114,7 @@ func (c *Canvas) drawScore(p1 int, p2 int) {
 
 func (c *Canvas) drawLine(l game.Player) {
 	c.context().Set("lineWidth", l.Width*c.W)
+	c.context().Set("strokeStyle", BallColor)
 	c.context().Call("beginPath")
 	c.context().Call("moveTo", l.Top.X*c.W, l.Top.Y*c.H)
 	c.context().Call("lineTo", l.Bottom.X*c.W, l.Bottom.Y*c.H)
@@ -119,13 +124,13 @@ func (c *Canvas) drawLine(l game.Player) {
 func (c *Canvas) drawBall(b game.Ball) {
 	c.context().Call("beginPath")
 	c.context().Call("arc", b.P.X*c.W, b.P.Y*c.H, b.R*c.W, 0, 3*math.Pi)
-	c.context().Set("fillStyle", "red")
+	c.context().Set("fillStyle", BallColor)
 	c.context().Call("fill")
 }
 
 func (c *Canvas) drawCountdown(restart time.Time) {
 	c.context().Set("font", "30px Arial")
-	c.context().Set("fillStyle", "black")
+	c.context().Set("fillStyle", TextColor)
 	c.context().Set("textAlign", "center")
 	c.context().Set("textBaseline", "top")
 	delta := restart.Sub(time.Now()).Seconds()
